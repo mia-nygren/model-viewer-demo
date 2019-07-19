96df86847337a7b56486799345eec65771523be8
Mia Nygren <marianygren88@gmail.com>
2019-07-19 04:09:25 +0200

    Updated home component

--- src/app/shared/components/home/home.component.ts
+++ src/app/shared/components/home/home.component.ts
@@ -5,6 +5,10 @@ import * as ukulele from '../../../../assets/models/ukulele/ukulele.gltf';
 import * as sofa from '../../../../assets/models/sofa/SOFA.gltf';
 import * as sofaImg from '../../../../assets/images/sofa.jpg';
 import * as ukeImg from '../../../../assets/images/ukulele.jpg';
+// Poster images ( for lazy loading and preload )
+import * as ukePoster from '../../../../assets/images/uke-poster.png';
+import * as sofaPoster from '../../../../assets/images/sofa-poster.png';
+import * as nightstandPoster from '../../../../assets/images/nightstand-poster.png';
 
 @Component({
     selector: 'home-page',
@@ -19,7 +23,18 @@ export class HomeComponent {
     ukeUrl:string = ukulele;
     sofaImage:string = sofaImg;
     ukeImage:string = ukeImg;
+    ukePoster:string = ukePoster;
+    sofaPoster:string = sofaPoster;
+    nightstandPoster:string = nightstandPoster;
+    loadingUke:boolean = false;
+    loadingSofa:boolean = false;
 
+    loadUke() {
+        this.loadingUke = !this.loadingUke;
+    }
+    loadSofa() {
+        this.loadingSofa = !this.loadingSofa;
+    }
     ngAfterViewInit() {
 
       }
